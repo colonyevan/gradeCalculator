87ae993389cbae2bc56cdc054fe72629efe81a9c
Evan Colony <colony.evan@gmail.com>
2019-11-25 17:00:27 -0500

    Restart

--- finalGradeSimple.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/*
-* Name: Evan Colony
-* Date: 12/21/2018
-* Purpose: 
-* Dependencies:
-*/
--- gradeNeededSimple.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-/*
-* Name: Evan Colony
-* Date: 12/21/2018
-* Purpose: These are all hard math functions, there is no predictive ability in any of these functions.
-* Dependencies:
-*/
-
-int gradeNeededCalculation();
